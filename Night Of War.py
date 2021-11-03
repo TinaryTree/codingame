3d7c61b41a11658d8d96f70d9fb49576a42bebff
q526933815 <32566921+q526933815@users.noreply.github.com>
2021-11-03 18:46:48 +0800

    Night Of War

---
+++ Night Of War.py
@@ -0,0 +1,111 @@
+# Jrke's special
+# -Kill your enemy soldiers or Have more bucks than your enemy at end of game
+import sys
+
+my_id = int(input())  # Your unique player Id
+map_size = int(input())  # the size of map MapSize*MapSize
+
+directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+move_directions = [(0, -1)]
+
+
+class WarMap:
+    def __init__(self, blocks: list):
+        self.blocks = blocks
+
+    def check(self, px, py):
+        for owner, _x, _y in self.blocks:
+            # print(f'check:{px}{py}', file=sys.stderr, flush=True)
+            # print(f'blocks:{self.blocks}', file=sys.stderr, flush=True)
+            if _x == px and _y == py:
+                return True
+        return False
+
+
+class Soldier:
+    def __init__(self, owner_id, x, y, soldier_id, level, direction, war_map: WarMap):
+        self.direction = direction
+        self.level = level
+        self.soldier_id = soldier_id
+        self.y = y
+        self.x = x
+        self.war_map = war_map
+        self.owner_id = owner_id
+
+    def cam_move(self):
+        res = []
+        # print(f'can_move:{self.x}{self.y}', file=sys.stderr, flush=True)
+        if war_map.check(self.x, self.y - 1) and self.direction != 2:
+            res.append('UP')
+        if war_map.check(self.x - 1, self.y) and self.direction != 3:
+            res.append('LEFT')
+        if war_map.check(self.x, self.y + 1) and self.direction != 0:
+            res.append('DOWN')
+        if war_map.check(self.x + 1, self.y) and self.direction != 1:
+            res.append('RIGHT')
+        return res
+
+    def around_pos(self):
+        positions = ((self.x + _x, self.y + _y) for _x, _y in directions)
+        positions = ((_x, _y) for _x, _y in positions if self.war_map.check(_x, _y))
+        if self.direction == 0:  # up
+            return [(_x, _y) for _x, _y in positions if _y <= self.y]
+        if self.direction == 1:  # LEFT
+            return [(_x, _y) for _x, _y in positions if _x <= self.x]
+        if self.direction == 2:  # DOWN
+            return [(_x, _y) for _x, _y in positions if _y >= self.y]
+        if self.direction == 3:  # RIGHT
+            return [(_x, _y) for _x, _y in positions if _x >= self.x]
+
+    def can_attack(self):
+        if self.direction == 0:  # up
+            return self.around_pos() + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y - 2)]
+        if self.direction == 1:  # LEFT
+            return self.around_pos() + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x - 2, self.y)]
+        if self.direction == 2:  # DOWN
+            return self.around_pos() + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y + 2)]
+        if self.direction == 3:  # RIGHT
+            return self.around_pos() + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x + 2, self.y)]
+
+
+# game loop
+while True:
+    my_bucks = int(input())  # Your Money
+    opp_bucks = int(input())  # Opponent Money
+    blocks = []
+    for i in range(map_size):
+        for j in range(map_size):
+            # block_owner: The playerId of this box owned player
+            # x: This block's position x
+            # y: This block's position y
+            block_owner, x, y = [int(k) for k in input().split()]
+            blocks.append((block_owner, x, y))
+    war_map = WarMap(blocks)
+    active_soldier_count = int(input())  # Total no. of active soldier in the game
+    my_soldiers = []
+    op_soldiers = []
+    for i in range(active_soldier_count):
+        # owner_id: owner of the soldier
+        # x: This soldier's position x
+        # y: This soldier's position y
+        # soldier_id: The unique identifier of soldier
+        # level: Level of the soldier ignore for first league
+        # direction: The side where the soldier is facing 0 = UP, 1 = LEFT , 2 = DOWN, 3 = RIGHT
+        owner_id, x, y, soldier_id, level, direction = [int(j) for j in input().split()]
+        soldier = Soldier(owner_id, x, y, soldier_id, level, direction, war_map)
+        if soldier.owner_id == my_id:
+            my_soldiers.append(soldier)
+        else:
+            op_soldiers.append(soldier)
+    for _ in my_soldiers:
+        for _op in op_soldiers:
+            print(f'{_op.x}{_op.y} att:{_.can_attack()}', file=sys.stderr, flush=True)
+            if (_op.x, _op.y) in _.can_attack():
+                print(f'ATTACK {_.soldier_id} {_op.soldier_id}')
+        print(f'{_.x}{_.y} d:{_.direction}', file=sys.stderr, flush=True)
+        print(f'MOVE {_.soldier_id} {_.cam_move()[0]} miao')
+
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    # print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE <opponent id> | SUICIDE <id>
+    # print("WAIT")
