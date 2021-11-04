fcec912b385eef0721ed47d49f7cb914f9919890
q526933815 <32566921+q526933815@users.noreply.github.com>
2021-11-04 17:47:18 +0800

    重构了结构，添加了被攻击评估，todo:极大极小策略

--- Night Of War.py
+++ Night Of War.py
@@ -9,49 +9,16 @@ map_size = int(input())  # the size of map MapSize*MapSize
 directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
 
 
-class WarMap:
-    def __init__(self, blocks: List[Tuple]):
-        self.blocks = blocks
-        self.pos = [(_x, _y) for owner, _x, _y in blocks]
-
-    def check(self, px, py, soldiers: list):
-        soldiers_pos = [(_.x, _.y) for _ in soldiers]
-        if ((px, py) in self.pos) and ((px, py) not in soldiers_pos):
-            return True
-        return False
-
-    def get_owner(self, px, py):
-        for owner, _x, _y in self.blocks:
-            if px == _x and py == _y:
-                return owner
-
-    def set_owner(self, px, py, owner):
-        pos = list(filter(lambda _: _[1] == px and _[2] == py, blocks))[0]
-        self.blocks.remove(pos)
-        self.blocks.append((owner, px, py))
-
-    def try_move(self, _soldier, soldier_move, owner_id):
-        if soldier_move == 'UP':
-            self.set_owner(_soldier.x, _soldier.y - 1, owner_id)
-        if soldier_move == 'LEFT':
-            self.set_owner(_soldier.x - 1, _soldier.y, owner_id)
-        if soldier_move == 'DOWN':
-            self.set_owner(_soldier.x, _soldier.y + 1, owner_id)
-        if soldier_move == 'RIGHT':
-            self.set_owner(_soldier.x + 1, _soldier.y, owner_id)
-
-
 class Soldier:
-    def __init__(self, owner_id, x, y, soldier_id, level, direction, war_map: WarMap):
+    def __init__(self, owner_id, x, y, soldier_id, level, direction):
         self.direction = direction
         self.level = level
         self.soldier_id = soldier_id
         self.y = y
         self.x = x
-        self.war_map = war_map
         self.owner_id = owner_id
 
-    def cam_move(self, soldiers):
+    def cam_move(self, soldiers, war_map):
         res = []
         if war_map.check(self.x, self.y - 1, soldiers) and self.direction != 2:
             res.append('UP')
@@ -63,9 +30,9 @@ class Soldier:
             res.append('RIGHT')
         return res
 
-    def around_pos(self):
+    def around_pos(self, war_map):
         positions = ((self.x + _x, self.y + _y) for _x, _y in directions)
-        positions = ((_x, _y) for _x, _y in positions if self.war_map.check(_x, _y, my_soldiers))
+        positions = ((_x, _y) for _x, _y in positions if war_map.check(_x, _y, my_soldiers))
         if self.direction == 0:  # up
             return [(_x, _y) for _x, _y in positions if _y <= self.y]
         if self.direction == 1:  # LEFT
@@ -75,20 +42,85 @@ class Soldier:
         if self.direction == 3:  # RIGHT
             return [(_x, _y) for _x, _y in positions if _x >= self.x]
 
-    def can_attack(self):
+    def can_attack_pos(self, war_map):
         if self.direction == 0:  # up
-            return self.around_pos() + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y - 2)]
+            return self.around_pos(war_map) + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y - 2)]
         if self.direction == 1:  # LEFT
-            return self.around_pos() + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x - 2, self.y)]
+            return self.around_pos(war_map) + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x - 2, self.y)]
         if self.direction == 2:  # DOWN
-            return self.around_pos() + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y + 2)]
+            return self.around_pos(war_map) + [(self.x + 2, self.y), (self.x - 2, self.y), (self.x, self.y + 2)]
         if self.direction == 3:  # RIGHT
-            return self.around_pos() + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x + 2, self.y)]
+            return self.around_pos(war_map) + [(self.x, self.y + 2), (self.x, self.y - 2), (self.x + 2, self.y)]
 
 
-def evaluate(map: WarMap, soldiers: List[Soldier]):
-    fx = {my_id: 1, -1: 0, 1-my_id: -1}
-    return sum((fx[_[0]] for _ in map.blocks))
+class WarMap:
+    def __init__(self, blocks: List[Tuple], _my_soldiers: List[Soldier], _op_soldiers: List[Soldier]):
+        self.op_soldiers = _op_soldiers
+        self.my_soldiers = _my_soldiers
+        self.blocks = blocks
+        self.pos = [(_x, _y) for owner, _x, _y in blocks]
+
+    def check(self, px, py, _my_soldiers: list):
+        soldiers_pos = [(_.x, _.y) for _ in _my_soldiers]
+        if ((px, py) in self.pos) and ((px, py) not in soldiers_pos):
+            return True
+        return False
+
+    def get_owner(self, px, py):
+        for owner, _x, _y in self.blocks:
+            if px == _x and py == _y:
+                return owner
+
+    def set_owner(self, _owner_id):
+        pos = list(filter(lambda _: _[1] == self.tx and _[2] == self.ty, self.blocks))[0]
+        self.blocks.remove(pos)
+        self.blocks.append((_owner_id, self.tx, self.ty))
+
+    def try_move(self, _soldier, soldier_move, _owner_id):
+        self.t_soldier = _soldier
+        if soldier_move == 'UP':
+            self.tx, self.ty = _soldier.x, _soldier.y - 1
+        if soldier_move == 'LEFT':
+            self.tx, self.ty = _soldier.x - 1, _soldier.y
+        if soldier_move == 'DOWN':
+            self.tx, self.ty = _soldier.x, _soldier.y + 1
+        if soldier_move == 'RIGHT':
+            self.tx, self.ty = _soldier.x + 1, _soldier.y
+        self.set_owner(_owner_id)
+        return self.score()
+
+    def dis_score(self):
+        """
+        距离分数，距离越近越好
+        :return:
+        """
+        op_pos = filter(lambda _: _[0] == 1 - my_id, self.blocks)
+        min_distance = min((abs(self.tx - x) + abs(self.ty - y) for _owner, _x, _y in op_pos))
+        return min_distance * (-2)
+
+    def map_score(self):
+        """
+        盘面分数
+        :return:自己的加一分，对方的减一分
+        """
+        fx = {my_id: 1, -1: 0, 1 - my_id: -1}
+        return sum((fx[_[0]] for _ in self.blocks))
+
+    def be_attacked(self):
+        """
+        被攻击指数
+        """
+        for _ in self.op_soldiers:
+            if (self.tx, self.ty) in _.can_attack_pos(self) and _.level >= self.t_soldier.level:
+                return -99999999
+        for m in self.my_soldiers:
+            for o in self.op_soldiers:
+                if (m.x, m.y) in o.can_attack_pos(self) and o.level >= m.level:
+                    return -99999999
+        return 0
+
+    def score(self):
+        return self.map_score() + self.be_attacked() + self.dis_score()
 
 
 # game loop
@@ -103,7 +135,7 @@ while True:
             # y: This block's position y
             block_owner, x, y = [int(k) for k in input().split()]
             blocks.append((block_owner, x, y))
-    war_map = WarMap(blocks)
+
     active_soldier_count = int(input())  # Total no. of active soldier in the game
     my_soldiers = []
     op_soldiers = []
@@ -115,26 +147,25 @@ while True:
         # level: Level of the soldier ignore for first league
         # direction: The side where the soldier is facing 0 = UP, 1 = LEFT , 2 = DOWN, 3 = RIGHT
         owner_id, x, y, soldier_id, level, direction = [int(j) for j in input().split()]
-        soldier = Soldier(owner_id, x, y, soldier_id, level, direction, war_map)
+        soldier = Soldier(owner_id, x, y, soldier_id, level, direction)
         if soldier.owner_id == my_id:
             my_soldiers.append(soldier)
         else:
             op_soldiers.append(soldier)
     ss = ''
-    my_map = WarMap(blocks)
+    my_map = WarMap(blocks, my_soldiers, op_soldiers)
     actions = []
     for _ in my_soldiers:
-        for move in _.cam_move(my_soldiers):
+        for move in _.cam_move(my_soldiers + op_soldiers, my_map):
             _map = copy.deepcopy(my_map)
-            _map.try_move(_, move, my_id)
-            score = evaluate(_map, op_soldiers)
+            score = _map.try_move(_, move, my_id)
             actions.append((score, f'MOVE {_.soldier_id} {move} {score}'))
 
     for _ in my_soldiers:
         for _op in op_soldiers:
             # print(f'{_op.x}{_op.y} att:{_.can_attack()}', file=sys.stderr, flush=True)
-            if (_op.x, _op.y) in _.can_attack():
-                actions.append((100, f'ATTACK {_.soldier_id} {_op.soldier_id}'))
+            if (_op.x, _op.y) in _.can_attack_pos(my_map) and _.level >= _op.level:
+                actions.append((999999999, f'ATTACK {_.soldier_id} {_op.soldier_id}'))
 
     print(max(actions)[1])
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
